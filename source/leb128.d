c6924c054389fd52f12ec0082695451f4b876e1f
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2021-10-07 01:43:55 +0100

    made endian-safe - now I only need to find a big-endian machine :)

--- source/leb128.d
+++ source/leb128.d
@@ -13,13 +13,22 @@ const(T) fromLEB128(T)(const ubyte[] buf, ref size_t processed) {
 			immutable size = T.sizeof * 8;
 		do {
 			b = buf[processed];
-			result |= to!T(b & 0x7f) << shift;
+			version (BigEndian) {
+				result |= to!T(b & 0x7f) << ((T.sizeof - 1) * 8 - shift);
+			} else {
+				result |= to!T(b & 0x7f) << shift;
+			}
 			shift += 7;
 			processed++;
 		} while (b & 0x80);
 		static if (isSigned!T)
-			if ((shift < size) && (b & 0x40) != 0)
-				result |= to!T(~0) << shift;
+			if ((shift < size) && (b & 0x40) != 0) {
+				version (BigEndian) {
+					result |= to!T(~0) << ((T.sizeof - 1) * 8 - shift);
+				} else {
+					result |= to!T(~0) << shift;
+				}
+			}
 		return result;
 	} else {
 		static assert(0, T.stringof ~ " is not an integral type");
@@ -37,13 +46,22 @@ void arrayFromLEB128(T)(const ubyte[] buf, ref size_t processed, ref T val) {
 			ubyte b;
 			do {
 				b = buf[n];
-				result |= to!(typeof(result))(b & 0x7f) << shift;
+				version (BigEndian) {
+					result |= to!(typeof(result))(b & 0x7f) << ((T.sizeof - 1) * 8 - shift);
+				} else {
+					result |= to!(typeof(result))(b & 0x7f) << shift;
+				}
 				shift += 7;
 				n++;
 			} while (b & 0x80);
 			static if (isSigned!(typeof(val[0])))
-				if ((shift < size) && (b & 0x40) != 0)
-					result |= to!(typeof(result))(~0) << shift;
+				if ((shift < size) && (b & 0x40) != 0) {
+					version (BigEndian) {
+						result |= to!(typeof(result))(~0) << ((T.sizeof - 1) * 8 - shift);
+					} else {
+						result |= to!(typeof(result))(~0) << shift;
+					}
+				}
 		}
 		processed = n;
 	} else {
@@ -54,15 +72,19 @@ void arrayFromLEB128(T)(const ubyte[] buf, ref size_t processed, ref T val) {
 /// Encode value `_val` of type `T` into given `buf`.
 void toLEB128(T)(OutBuffer buf, const T _val) {
 	static if (isScalarType!T && !isFloatingPoint!T) {
+		ubyte[T.sizeof*2] data;
 		bool more = true;
-		ubyte[32] d;
 		size_t idx;
 		static if (isSigned!T)
 			long val = _val;
 		else
 			ulong val = _val;
 		do {
-			ubyte b = val & 0x7f;
+			version (BigEndian) {
+				ubyte b = (cast(ubyte[T.sizeof])((cast(ubyte*)&val)[0..T.sizeof]))[T.sizeof-1] & 0x7f;
+			} else {
+				ubyte b = val & 0x7f;
+			}
 			val >>= 7;
 			static if (isSigned!T) {
 				if ((val == 0 && (b & 0x40) == 0) || (val == -1 && (b & 0x40) != 0))
@@ -75,13 +97,13 @@ void toLEB128(T)(OutBuffer buf, const T _val) {
 				else
 					more = false;
 			}
-			d[idx] = b;
+			data[idx] = b;
 			idx++;
 		} while (more);
 		if (idx > 0)
-			buf.write(d[0..idx]);
+			buf.write(data[0..idx]);
 	} else static if (isArray!T && !isFloatingPoint!(typeof(_val[0]))) {
-		ubyte[8192] d;
+		ubyte[8192] data;
 		size_t idx;
 		foreach (v; _val) {
 			bool more = true;
@@ -90,7 +112,11 @@ void toLEB128(T)(OutBuffer buf, const T _val) {
 			else
 				ulong val = v;
 			do {
-				ubyte b = val & 0x7f;
+				version (BigEndian) {
+					ubyte b = (cast(ubyte[T.sizeof])((cast(ubyte*)&val)[0..T.sizeof]))[T.sizeof-1] & 0x7f;
+				} else {
+					ubyte b = val & 0x7f;
+				}
 				val >>= 7;
 				static if (isSigned!(typeof(v))) {
 					if ((val == 0 && (b & 0x40) == 0) || (val == -1 && (b & 0x40) != 0))
@@ -103,15 +129,15 @@ void toLEB128(T)(OutBuffer buf, const T _val) {
 					else
 						more = false;
 				}
-				d[idx] = b;
-				if (++idx >= d.length) {
-					buf.write(d);
+				data[idx] = b;
+				if (++idx >= data.length) {
+					buf.write(data);
 					idx = 0;
 				}
 			} while (more);
 		}
 		if (idx > 0)
-			buf.write(d[0..idx]);
+			buf.write(data[0..idx]);
 	} else {
 		static assert(0, T.stringof ~ " is not an integral type");
 	}
